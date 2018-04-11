e8c562d830505463c0c32bc669678d957f2a3a2b
Lola Simpson <lolalsimp@gmail.com>
2018-04-10 20:12:54 -0500

    addSub

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,9 @@
-console.log('Hi');
+const add = require('./add');
+const sub = require('./subtract');
+const multiply = require('./multiply');
+const divide = require('./divide');
+
+console.log('Adding: ', add(1, 2));
+console.log('Subtracting: ', sub(40, 2));
+console.log('Multiplying:', multiply(40, 1));
+console.log('Divide: ', divide(12, 2));
